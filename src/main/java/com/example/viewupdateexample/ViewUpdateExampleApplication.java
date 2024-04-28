c46e2234429756c7b563cc56a151d1f72ebaa684
klc-yckim <yckim@korealaundry.com>
2024-04-29 07:34:40 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/com/example/viewupdateexample/ViewUpdateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.viewupdateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewUpdateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewUpdateExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viewupdateexample/ViewUpdateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viewupdateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewUpdateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
